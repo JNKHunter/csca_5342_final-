b9acf96bd08699859290ccc5ca5020f0bbbbdd47
JNKHunter <dever@system76-pc.localdomain>
2025-01-25 17:50:49 -0500

    successful round trip

--- controllers/main_controller/init_object_manip.py
+++ controllers/main_controller/init_object_manip.py
@@ -6,22 +6,23 @@ class InitObjectManip(Behaviour):
 
         super(InitObjectManip, self).__init__(name)
         self.robot = blackboard.get('robot')
+        self.robot_node = self.robot.getSelf()
         self.blackboard = blackboard
 
         self.has_run = False
     def update(self):
         if not self.has_run:
-            translation_field = self.robot.getField("translation")
-            rotation_field = self.robot.getField("rotation")
+            translation_field = self.robot_node.getField("translation")
+            rotation_field = self.robot_node.getField("rotation")
 
             # Set new translation (x, y, z)
             new_translation = [0.0, 0.18, 0.095]  # Example: Move to (1.0, 0.0, 1.0)
             translation_field.setSFVec3f(new_translation)
 
             # Set new rotation (axis-x, axis-y, axis-z, angle in radians)
-            axis = [0, 0, 1]  # Rotate around the Y-axis
+            new_rotation = [0, 0, 1, 0]  # Rotate around the Y-axis
             
-            rotation_field.setSFRotation(0)
+            rotation_field.setSFRotation(new_rotation)
             self.has_run = True
             return Status.SUCCESS
         
