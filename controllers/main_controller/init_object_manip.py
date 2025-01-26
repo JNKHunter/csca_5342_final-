fc0c25034a1b59e6cd7c48805c67f2166c7209c0
JNKHunter <dever@system76-pc.localdomain>
2025-01-26 07:33:55 -0500

    2nd jar placement working again

--- controllers/main_controller/init_object_manip.py
+++ controllers/main_controller/init_object_manip.py
@@ -15,12 +15,9 @@ class InitObjectManip(Behaviour):
             translation_field = self.robot_node.getField("translation")
             rotation_field = self.robot_node.getField("rotation")
 
-            # Set new translation (x, y, z)
-            new_translation = [0.0, 0.18, 0.095]  # Example: Move to (1.0, 0.0, 1.0)
+            new_translation = [0.0, 0.18, 0.095] 
             translation_field.setSFVec3f(new_translation)
-
-            # Set new rotation (axis-x, axis-y, axis-z, angle in radians)
-            new_rotation = [0, 0, 1, 0]  # Rotate around the Y-axis
+            new_rotation = [0, 0, 1, 0]
             
             rotation_field.setSFRotation(new_rotation)
             self.has_run = True
