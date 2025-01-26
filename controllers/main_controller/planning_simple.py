f8f0d31216f269bf56c57463252ae675c018da65
JNKHunter <dever@system76-pc.localdomain>
2025-01-26 14:32:37 -0500

    updated documentation

--- controllers/main_controller/planning_simple.py
+++ controllers/main_controller/planning_simple.py
@@ -14,10 +14,6 @@ class PlanningSimple(Behaviour):
     def update(self):
         return Status.SUCCESS
 
-    '''
-    When this behavior is complete, prepare the shortest path found and save to the blackboard.
-    This shortest path will be used by the next navigation behavior.
-    '''
     def terminate(self,new_status):
             self.logger.debug(f'PlanningSimple::terminate {self.name}')
             self.blackboard['waypoints'] = self.waypoints
